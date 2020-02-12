57c63cbd8fa8379944fd1bd810c476078e20858b
Jesse Connell <ancon@upenn.edu>
2020-02-12 14:53:40 -0500

    more reporting rules

---
+++ inst/python/igseq/reporting.py
@@ -0,0 +1,76 @@
+"""
+Summarizing and reporting helper functions.
+"""
+import csv
+from igseq.data import load_csv
+
+def counts_sample_summary(
+        file_counts_in, counts_sample_summary_out, samples, runs, samples_per_run):
+    """Take a list of sequence counts for fastq.gz files and summarize per-sample."""
+    counts = load_csv(file_counts_in)
+    rows = []
+    for runid in runs.keys():
+        for sample in samples_per_run[runid] + ["unassigned"]:
+            key = "counts/demux-prev/{run}/{sample}.I1.fastq.gz.counts".format(
+                run=runid,
+                sample=sample)
+            cts = int(counts[key]["NumSequences"])
+            try:
+                cellcount = samples[sample]["SpecimenAttrs"]["CellCount"]
+                ratio = divide(cts, cellcount)
+            except KeyError:
+                cellcount = ""
+                ratio = ""
+            rows.append({
+                "Run": runid,
+                "Sample": sample,
+                "CellCount": cellcount,
+                "NumSequences": cts,
+                "Ratio": ratio})
+    with open(counts_sample_summary_out, "wt") as f_out:
+        writer = csv.DictWriter(
+            f_out,
+            fieldnames=["Run", "Sample", "NumSequences", "CellCount", "Ratio"])
+        writer.writeheader()
+        writer.writerows(rows)
+
+def counts_run_summary(counts_sample_summary_in, counts_run_summary_out):
+    """Take a per-sample summary of sequence counts and make a per-run version."""
+    with open(counts_sample_summary_in) as f_in:
+        reader = csv.DictReader(f_in)
+        counts_by_sample = list(reader)
+    counts_by_run = {}
+    for row_in in counts_by_sample:
+        runid = row_in["Run"]
+        try:
+            cts = counts_by_run[runid]
+        except KeyError:
+            counts_by_run[runid] = {"samples": 0, "unassigned": 0}
+            cts = counts_by_run[runid]
+        if row_in["Sample"] == "unassigned":
+            key = "unassigned"
+        else:
+            key = "samples"
+        cts[key] += int(row_in["NumSequences"])
+    rows = []
+    for run_name, run_attrs in counts_by_run.items():
+        try:
+            ratio = divide(run_attrs["unassigned"], run_attrs["samples"])
+        except KeyError:
+            ratio = ""
+        rows.append([
+            run_name,
+            run_attrs.get("unassigned", ""),
+            run_attrs.get("samples", ""),
+            run_attrs.get("unassigned", "") + run_attrs.get("samples", ""),
+            ratio])
+    with open(counts_run_summary_out, "wt") as f_out:
+        writer = csv.writer(f_out)
+        writer.writerow(["Run", "UnassignedSeqs", "SampleSeqs", "TotalSeqs", "Ratio"])
+        writer.writerows(rows)
+
+def divide(val1, val2, fmt="{:.6f}"):
+    """Divide val1 by val2 as floats and return formatted string."""
+    num = float(val1)/float(val2)
+    num = fmt.format(num)
+    return num
--- inst/snakemake-rules/reporting.smk
+++ inst/snakemake-rules/reporting.smk
@@ -1,26 +1,34 @@
 ### Reporting
 
-rule fastq_gz_counts:
-    output: "{prefix}.fastq.gz.counts"
-    input: "{prefix}.fastq.gz"
-    shell: "zcat {input} | wc -l | awk -v OFS=, -v f={input} '{{print f,$0/4}}' > {output}"
+from igseq.reporting import counts_sample_summary, counts_run_summary
 
-rule fasta_gz_counts:
-    output: "{prefix}.fasta.gz.counts"
-    input: "{prefix}.fasta.gz"
-    shell: "zgrep -c '^>' {input} | awk -v OFS=, -v f={input} '{{print f,$0}}' > {output}"
 
-rule fasta_counts:
-    output: "{prefix}.fasta.counts"
-    input: "{prefix}.fasta"
-    shell: "grep -c '^>' {input} | awk -v OFS=, -v f={input} '{{print f,$0}}' > {output}"
+TARGET_REPORT_COUNTS = []
+for runid in RUNS.keys():
+    TARGET_REPORT_COUNTS.extend(expand(
+        "counts/demux-prev/{run}/{sample}.{rp}.fastq.gz.counts",
+        sample=SAMPLES_PER_RUN[runid] + ["unassigned"],
+        run=runid,
+        rp=["R1", "R2", "I1"]))
 
 rule counts_table:
-    output: "counts.csv"
-    input:
-        raw=expand("0-data/{run}/" + RAW + ".counts", run = RUNS.keys(), rp = ["R1"])
+    """Just a big ol' list of file paths and read counts."""
+    output: "reporting/counts.csv"
+    input: TARGET_REPORT_COUNTS
     shell:
         """
             echo "Filename,NumSequences" > {output}
-            cat {input} | sort >> {output}
+            paste -d , <(echo "{input}" | tr ' ' '\\n') <(cat {input}) >> {output}
         """
+
+rule counts_sample_summary:
+    """A per-sample summary of raw read counts."""
+    output: "reporting/counts_by_sample.csv"
+    input: "reporting/counts.csv"
+    run: counts_sample_summary(input[0], output[0], SAMPLES, RUNS, SAMPLES_PER_RUN)
+
+rule counts_run_summary:
+    """A per-run summary of raw read counts."""
+    output: "reporting/counts_by_run.csv"
+    input: "reporting/counts_by_sample.csv"
+    run: counts_run_summary(input[0], output[0])
