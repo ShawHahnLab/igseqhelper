5d3eeb94c9659dfef32bbbed1461a35c4b42c4ef
Jesse Connell <ancon@upenn.edu>
2020-03-09 15:30:33 -0400

    Python pkg: sonar: update tests

--- inst/python/test_igseq/test_sonar.py
+++ inst/python/test_igseq/test_sonar.py
@@ -19,23 +19,20 @@ class TestSonar(unittest.TestCase):
     def test_gather_germline(self):
         """Test gathering germline sequences for SONAR.
 
-        This should take in an IMGT FASTA file and optionally a second set of
-        references and output a FASTA with sequence IDs formatted the way SONAR
-        wants.
+        This should take in a FASTA file and output a FASTA with sequence IDs
+        formatted the way SONAR wants.
         """
-        segment = "V"
-        input_imgt_fp = INST/"reference/imgt/IGHV.fasta"
-        input_extra_v_fp = INST/"reference/10.1016_j.cell.2019.06.030/tables3c.heavy.fasta"
+        input_fps = [INST/"reference/imgt/IGHV.fasta"]
         with TemporaryDirectory() as outdir:
             output_fp = Path(outdir) / "output.fasta"
-            gather_germline(input_imgt_fp, input_extra_v_fp, output_fp, segment)
+            gather_germline(input_fps, output_fp)
             parser = SeqIO.parse(output_fp, "fasta")
             # first and last records
             record1 = next(parser)
             for record in parser:
                 record2 = record
         self.assertEqual(record1.id, "IGHV1-1*01")
-        self.assertEqual(record2.id, "IGHV7-AGO-S*01_S4447")
+        self.assertEqual(record2.id, "IGHV7-1*01")
 
     def test_munge_seqid_for_sonar(self):
         """Test modifying sequence IDs for SONAR."""
