559e81fa7605fe1af9b88d05ba0b8fb6eac574eb
Jesse Connell <ancon@upenn.edu>
2020-04-06 12:04:52 -0400

    Python pkg: igdiscover: comments

--- inst/python/igseq/igdiscover.py
+++ inst/python/igseq/igdiscover.py
@@ -4,7 +4,8 @@ Helpers for running IgDiscover.
 
 # Mapping of chain heavy/light names to heavy/light short specifiers
 CHAINS = {"heavy": "H", "light": "L"}
-# Mapping of chain types to heavy/light short specifiers
+# Mapping of chain types to heavy/light short specifiers per locus.  Note the
+# potential confusion between L here and L above though.
 CHAIN_TYPES = {
     "alpha": "H",
     "delta": "H",
